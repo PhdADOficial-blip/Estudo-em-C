033674a1b69b74dff659a862e9a567158001af0f
Pedro Henrique <pedroalencaroficial@gmail.com>
2026-05-16 18:41:28 -0300

    media de alunos
    hora de codar media dos alunos

--- tema2-aventureiro/hora-de-codar-exercicio1.c
+++ tema2-aventureiro/hora-de-codar-exercicio1.c
@@ -3,24 +3,45 @@
 
         int main () {
               
-             int nota1 = 8;
-             int nota2 = 10;
-             int nota3 = 6;
-             float soma = (float) nota1 + nota2 + nota3;
-             float media= (float) soma / 3;
+             int  nota1, nota2, nota3 ;
+             float media;
 
+            printf("---------------------\n");
+            printf("---------------------\n");
+            printf("----------media de alunos modelo 1-----------\n");
+            1
+            printf("nota1: \n");
+            scanf("%d", &nota1);
 
-             printf("media das notas são: %d" , media);
-     
+            printf("nota2: \n");
+            scanf("%d", &nota2);
 
+            printf("nota3:\n");
+            scanf("%d", &nota3);
+            
+            media = (float) (nota1 + nota2 + nota3) / 3;
 
+            printf("media das notas e: %.1f \n" , media);
+             
 
+            float  nota11, nota22, nota33 ;
+            float media1;
 
+            printf("---------------------\n");
+            printf("----------media de alunos modelo 2-----------\n");
+            printf("---------------------\n");
+            printf("nota1: \n");
+            scanf("%f", &nota11);
 
+            printf("nota2: \n");
+            scanf("%f", &nota22);
 
+            printf("nota3:\n");
+            scanf("%f", &nota33);
+            
+            media1 = (float) (nota11 + nota22 + nota33) / 3;
 
-
-
+            printf("media das notas e: %.1f" , media1);
 
 
 

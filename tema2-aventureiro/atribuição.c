d546263cf4e72407d456e3db561e142445190ecd
Pedro Henrique <pedroalencaroficial@gmail.com>
2026-05-16 16:18:29 -0300

    atualização matematica c

---
+++ tema2-aventureiro/atribuição.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    /*
+    Atribuição simples 
+    Atribuição com soma 
+    Atribuição com subtração 
+    Atribuiçõa com multiplicação 
+    Atribuição com divisão
+    */
+    int numero1 = 10, numnero2, resultado;
+    resultado = 10;
+    printf("resultado: %d \n", resultado);
+
+    resultado += 20;
+    printf("resultado: %d \n", resultado);
+
+  
+    return 0;
+}

34f14a1f15b0ff059c586125382f63a8da437f2b
Pedro Henrique <pedroalencaroficial@gmail.com>
2026-05-16 19:22:10 -0300

    super trunfo parte 2
    calculo

--- desafio/Nivel novato.c
+++ desafio/Nivel novato.c
@@ -21,20 +21,29 @@ int main() {
     char estado1; 
     char codigo1[10] = "i";
     char cidade1[50] = "i";
-    unsigned long int populacao1 = 1;
+    int populacao1 = 1;
     float area1 = 2;
     float pib1= 2;
     int pontosTuristicos1 = 2;
+    float densidadepopu1;
+    float pibpercapita1;
+    
+
 
     // Carta 2
     char estado2 ;
     char codigo2[10] = "r";
     char cidade2[50] = "e";
-    unsigned long int populacao2 = 3 ;
+    int populacao2 = 3 ;
     float area2 = 3 ;
     float pib2 = 4;
     int pontosTuristicos2 = 5;
- 
+    float densidadepopu2;
+    float pibpercapita2;
+    
+
+
+
      // Entrada Carta 1
     printf("=== Cadastro da Carta 1 ===\n");
 
@@ -48,7 +57,7 @@ int main() {
     scanf("%s", cidade1);
 
     printf("Populacao: ");
-    scanf("%lu", &populacao1);
+    scanf("%d", &populacao1);
 
     printf("Area: ");
     scanf("%f", &area1);
@@ -72,7 +81,7 @@ int main() {
     scanf("%s", cidade2);
 
     printf("Populacao: ");
-    scanf("%lu", &populacao2);
+    scanf("%d", &populacao2);
 
     printf("Area: ");
     scanf("%f", &area2);
@@ -83,27 +92,47 @@ int main() {
     printf("Numero de pontos turisticos: ");
     scanf("%d", &pontosTuristicos2);
     
+
+    /*1- calculo da densidade populacional: população / area = densidade (kmquadrado)
+    
+      2 pib per capita PIB / população = PIB per capita (float)*/
+    
+
+    //calculo pib percapita cidade 1
+    pibpercapita1 = pib1 / populacao1;
+    //calculo pib percapita cidade 2
+    pibpercapita2 = pib2 / populacao2;
+ 
+    //Densidade Populacional: 8102.47 hab/km²
+
+    densidadepopu1 = populacao1 / populacao1;
+    densidadepopu2 = populacao2 / populacao2;
+
+
+
      // Exibição Carta 1
     printf("\n=== Carta 1 ===\n");
     printf("Estado: %c\n", estado1);
     printf("Codigo: %s\n", codigo1);
     printf("Cidade: %s\n", cidade1);
-    printf("Populacao: %lu\n", populacao1);
+    printf("Populacao: %d\n", populacao1);
     printf("Area: %.2f\n", area1);
     printf("PIB: %.2f\n", pib1);
     printf("Pontos Turisticos: %d\n", pontosTuristicos1);
-    
+    printf("O PIB per Capita é: %.2f Reais\n", pibpercapita1);
+    printf("A densidade populacional é: %.2f km.\n", densidadepopu1);
+
 
     // Exibição Carta 2
     printf("\n=== Carta 2 ===\n");
     printf("Estado: %c\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Cidade: %s\n", cidade2);
-    printf("Populacao: %lu\n", populacao2);
+    printf("Populacao: %d\n", populacao2);
     printf("Area: %.2f\n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Pontos Turisticos: %d\n", pontosTuristicos2);
-    
-
+    printf("O PIB per Capita é: %.2f Reais" ,pibpercapita2);
+    printf("A densidade populacional é: %.2f km.\n", densidadepopu2);
     return 0;
 }
